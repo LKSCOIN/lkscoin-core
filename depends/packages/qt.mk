b93cd3b1f89d81a35852410fc6e907f9451602e2
LKS Foundation <info.marcominin@gmail.com>
2026-08-21 22:44:33 +0200

    depends: fetch Qt 5.12.11 from download.qt.io/archive (official_releases returns 404)

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -1,6 +1,10 @@
 PACKAGE=qt
 $(package)_version=5.12.11
-$(package)_download_path=https://download.qt.io/official_releases/qt/5.12/$($(package)_version)/submodules
+# Qt moves superseded point releases out of official_releases/ into archive/,
+# where they stay available. 5.12.11 was moved and the upstream URL now returns
+# 404, breaking the depends build; archive/ serves the same tarballs and the
+# sha256 hashes below still verify.
+$(package)_download_path=https://download.qt.io/archive/qt/5.12/$($(package)_version)/submodules
 $(package)_suffix=everywhere-src-$($(package)_version).tar.xz
 $(package)_file_name=qtbase-$($(package)_suffix)
 $(package)_sha256_hash=1c1b4e33137ca77881074c140d54c3c9747e845a31338cfe8680f171f0bc3a39
